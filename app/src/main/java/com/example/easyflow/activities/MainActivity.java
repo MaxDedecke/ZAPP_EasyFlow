a2b1f67c43862e772b9f772ab3e4b71da9f33fb5
stefan bauer <stefan.bauer08web.de>
2019-05-31 08:47:07 +0200

    Anzeige der Icons gefixed und Auswahl der Konten in der Actionbar hinzugefügt

--- app/src/main/java/com/example/easyflow/activities/MainActivity.java
+++ app/src/main/java/com/example/easyflow/activities/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.easyflow.activities;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.ActionMenuItemView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -16,9 +20,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.easyflow.R;
@@ -31,7 +32,6 @@ import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.firebase.ui.database.SnapshotParser;
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 
 import java.text.SimpleDateFormat;
@@ -41,46 +41,38 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    public static List<Category> categoriesIncome=new ArrayList<>();
-    public static List<Category> categoriesCost=new ArrayList<>();
+    public static List<Category> categoriesIncome = new ArrayList<>();
+    public static List<Category> categoriesCost = new ArrayList<>();
 
 
-
-
-    private RecyclerView recyclerView;
-    private LinearLayoutManager linearLayoutManager;
-    private FirebaseRecyclerAdapter adapter;
-
+    private RecyclerView mRecyclerView;
+    private LinearLayoutManager mLinearLayoutManager;
+    private FirebaseRecyclerAdapter mFirebaseRecyclerAdapter;
+    private Toolbar mToolbar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
 
 
-        //todo implement livedata
-        FirebaseHelper helper= FirebaseHelper.getInstance();
-        //helper.setLiveDataListener();
-        //AdapterRecyclerViewOverview adapter = new AdapterRecyclerViewOverview(this, items);
-        //mRecyclerView.setAdapter(adapter);
-
-        recyclerView = findViewById(R.id.list);
-        linearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerView.setHasFixedSize(true);
+        // Initialize and show LiveData for the Main Content
+        mRecyclerView = findViewById(R.id.list);
+        mLinearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
+        mRecyclerView.setHasFixedSize(true);
         fetch();
-
 
 
         loadCategories();
@@ -88,24 +80,24 @@ public class MainActivity extends AppCompatActivity
 
     private void loadCategories() {
         // Kategorien für Ausgaben.
-        categoriesCost.add(new Category(getString(R.string.categoriy_reisen),R.drawable.ic_airplane_brown_24dp));
-        categoriesCost.add(new Category(getString(R.string.categoriy_auto),R.drawable.ic_car_darkblue_24dp));
-        categoriesCost.add(new Category(getString(R.string.categoriy_kommunikation),R.drawable.ic_communication_24dp));
-        categoriesCost.add(new Category(getString(R.string.categoriy_eating),R.drawable.ic_eating_green_24dp));
-        categoriesCost.add(new Category(getString(R.string.categoriy_lebensmittel),R.drawable.ic_food_24dp));
-        categoriesCost.add(new Category(getString(R.string.categoriy_gesundheit),R.drawable.ic_health_24dp));
-        categoriesCost.add(new Category(getString(R.string.categoriy_haus),R.drawable.ic_home_24dp));
-        categoriesCost.add(new Category(getString(R.string.categoriy_taxi),R.drawable.ic_local_taxi_black_24dp));
-        categoriesCost.add(new Category(getString(R.string.categoriy_geschenke),R.drawable.ic_present_24dp));
-        categoriesCost.add(new Category(getString(R.string.categoriy_sport),R.drawable.ic_sport_24dp));
-        categoriesCost.add(new Category(getString(R.string.categoriy_verkehrsmittel),R.drawable.ic_traffic_24dp));
-        categoriesCost.add(new Category(getString(R.string.category_bildung),R.drawable.ic_school_black_24dp));
+        categoriesCost.add(new Category(getString(R.string.categoriy_reisen), R.drawable.ic_airplane_brown_24dp));
+        categoriesCost.add(new Category(getString(R.string.categoriy_auto), R.drawable.ic_car_darkblue_24dp));
+        categoriesCost.add(new Category(getString(R.string.categoriy_kommunikation), R.drawable.ic_communication_24dp));
+        categoriesCost.add(new Category(getString(R.string.categoriy_eating), R.drawable.ic_eating_green_24dp));
+        categoriesCost.add(new Category(getString(R.string.categoriy_lebensmittel), R.drawable.ic_food_24dp));
+        categoriesCost.add(new Category(getString(R.string.categoriy_gesundheit), R.drawable.ic_health_24dp));
+        categoriesCost.add(new Category(getString(R.string.categoriy_haus), R.drawable.ic_home_24dp));
+        categoriesCost.add(new Category(getString(R.string.categoriy_taxi), R.drawable.ic_local_taxi_black_24dp));
+        categoriesCost.add(new Category(getString(R.string.categoriy_geschenke), R.drawable.ic_present_24dp));
+        categoriesCost.add(new Category(getString(R.string.categoriy_sport), R.drawable.ic_sport_24dp));
+        categoriesCost.add(new Category(getString(R.string.categoriy_verkehrsmittel), R.drawable.ic_traffic_24dp));
+        categoriesCost.add(new Category(getString(R.string.category_bildung), R.drawable.ic_school_black_24dp));
 
 
         // Kategorien für Einkommen.
-        categoriesIncome.add(new Category(getString(R.string.categoriy_einzahlungen),R.drawable.ic_einzahlungen_24dp));
-        categoriesIncome.add(new Category(getString(R.string.categoriy_ersparnisse),R.drawable.ic_trending_up_black_24dp));
-        categoriesIncome.add(new Category(getString(R.string.categoriy_gehalt),R.drawable.ic_gehalt_24dp));
+        categoriesIncome.add(new Category(getString(R.string.categoriy_einzahlungen), R.drawable.ic_einzahlungen_24dp));
+        categoriesIncome.add(new Category(getString(R.string.categoriy_ersparnisse), R.drawable.ic_trending_up_black_24dp));
+        categoriesIncome.add(new Category(getString(R.string.categoriy_gehalt), R.drawable.ic_gehalt_24dp));
     }
 
     @Override
@@ -125,6 +117,7 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -132,12 +125,34 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_account_current) {
             return true;
         }
 
+
+        if (id == R.id.action_account_second) {
+            MenuItem head=mToolbar.getMenu().getItem(1);
+            swapItems(item, head);
+        }
+        else if (id == R.id.action_account_third) {
+            MenuItem head=mToolbar.getMenu().getItem(1);
+            swapItems(item, head);
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private void swapItems(MenuItem item, MenuItem head) {
+        String tmpString=head.getTitle().toString();
+        Drawable tmpDraw= head.getIcon();
+
+        head.setTitle(item.getTitle());
+        head.setIcon(item.getIcon());
+
+        item.setTitle(tmpString);
+        item.setIcon(tmpDraw);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -166,14 +181,13 @@ public class MainActivity extends AppCompatActivity
     }
 
     public void showMoveActivity(View view) {
-        int viewId=view.getId();
+        int viewId = view.getId();
 
-        Intent newIntent=new Intent(MainActivity.this, EinAusgabeActivity.class);
+        Intent newIntent = new Intent(MainActivity.this, EinAusgabeActivity.class);
 
-        if(viewId==R.id.btnEinnahme){
-            newIntent.putExtra(getString(R.string.key_show_ein_or_ausgabe),true);
-        }
-        else if(viewId==R.id.btnAusgabe) {
+        if (viewId == R.id.btnEinnahme) {
+            newIntent.putExtra(getString(R.string.key_show_ein_or_ausgabe), true);
+        } else if (viewId == R.id.btnAusgabe) {
             newIntent.putExtra(getString(R.string.key_show_ein_or_ausgabe), false);
         }
 
@@ -182,22 +196,16 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-
-
-
-
-
-
     @Override
     protected void onStart() {
         super.onStart();
-        adapter.startListening();
+        mFirebaseRecyclerAdapter.startListening();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        adapter.stopListening();
+        mFirebaseRecyclerAdapter.stopListening();
     }
 
 
@@ -216,7 +224,7 @@ public class MainActivity extends AppCompatActivity
                         })
                         .build();
 
-        adapter = new FirebaseRecyclerAdapter<Cost, ViewHolder>(options) {
+        mFirebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Cost, ViewHolder>(options) {
             @Override
             public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
                 View view = LayoutInflater.from(parent.getContext())
@@ -226,25 +234,23 @@ public class MainActivity extends AppCompatActivity
             }
 
 
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             protected void onBindViewHolder(ViewHolder holder, final int position, Cost cost) {
                 holder.setTxtTitle(cost.getCategory().getName());
-                holder.setTxtDesc(cost.getValue()+" - "+new SimpleDateFormat(Constants.DATE_FORMAT_WEEKDAY).format(cost.getDate()));
-                //todo set sum of costs holder.setTxtDesc(cost.getDate().toString());
+                holder.setImageViews(cost.getCategory());
+                holder.setTxtDesc(new SimpleDateFormat(Constants.DATE_FORMAT_WEEKDAY).format(cost.getDate()) + " - " + cost.getNote());
+                holder.setTxtValue(cost.getValue());
 
-                holder.root.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Toast.makeText(MainActivity.this, String.valueOf(position), Toast.LENGTH_SHORT).show();
-                    }
+                holder.root.setOnClickListener(view -> {
+                    //todo
+                    Toast.makeText(MainActivity.this, String.valueOf(position), Toast.LENGTH_SHORT).show();
                 });
             }
 
         };
-        recyclerView.setAdapter(adapter);
+        mRecyclerView.setAdapter(mFirebaseRecyclerAdapter);
     }
-
-
 
 
 }
--- app/src/main/java/com/example/easyflow/interfaces/ViewHolder.java
+++ app/src/main/java/com/example/easyflow/interfaces/ViewHolder.java
@@ -1,38 +1,105 @@
 package com.example.easyflow.interfaces;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.easyflow.R;
+import com.example.easyflow.activities.MainActivity;
+import com.example.easyflow.models.Category;
+import com.example.easyflow.models.Cost;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 public class ViewHolder extends RecyclerView.ViewHolder {
     public LinearLayout root;
-    public TextView txtTitle;
-    public TextView txtDesc;
+    public TextView mTxtTitle;
+    public TextView mTxtDesc;
+    public TextView mTxtValue;
+    public ImageView mIVCategory;
+    public ImageView mIVArrow;
 
     // todo make code and view specific for costs
 
     public ViewHolder(View itemView) {
         super(itemView);
         root = itemView.findViewById(R.id.list_root);
-        txtTitle = itemView.findViewById(R.id.list_title);
-        txtDesc = itemView.findViewById(R.id.list_desc);
+        mTxtTitle = itemView.findViewById(R.id.list_title);
+        mTxtDesc = itemView.findViewById(R.id.list_desc);
+        mTxtValue = itemView.findViewById(R.id.list_value);
+        mIVArrow = itemView.findViewById(R.id.list_arrow);
+        mIVCategory = itemView.findViewById(R.id.list_category);
+        /*
         itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //todo wenn benötigt
 
             }
         });
+        */
     }
 
     public void setTxtTitle(String string) {
-        txtTitle.setText(string);
+        mTxtTitle.setText(string);
     }
 
 
     public void setTxtDesc(String string) {
-        txtDesc.setText(string);
+        mTxtDesc.setText(string);
+    }
+
+    public void setTxtValue(Double value) {
+        mTxtValue.setText(String.format("%.2f",value));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void setImageViews(final Category category) {
+        //todo get right iconid from database object
+        //todo find object with stream and string value
+
+        Context context = root.getContext();
+        Category matching = null;
+
+        Iterator<Category> iteratorCost = MainActivity.categoriesCost.iterator();
+        while (iteratorCost.hasNext() && matching == null) {
+            Category c = iteratorCost.next();
+            if (c.getId() == category.getId())
+                matching = c;
+        }
+
+
+        if (matching != null) {
+            mIVArrow.setImageDrawable(context.getDrawable(R.drawable.ic_arrow_downward_red_32dp));
+            mIVCategory.setImageDrawable(context.getDrawable(matching.getIconId()));
+            return;
+        }
+
+
+        Iterator<Category> iteratorIncome = MainActivity.categoriesIncome.iterator();
+        while (iteratorIncome.hasNext() && matching == null) {
+            Category c = iteratorIncome.next();
+            if (c.getId() == category.getId())
+                matching = c;
+        }
+
+        if (matching != null) {
+            mIVArrow.setImageDrawable(context.getDrawable(R.drawable.ic_arrow_upward_green_24dp));
+            mIVCategory.setImageDrawable(context.getDrawable(matching.getIconId()));
+        }
+
+
+        //todo find iconid in list for ausgabe or einnahme
+
     }
 }
