886b40a2705c524353f1eebaaba630de7952ae53
MaxDedecke <jm.dedecke@gmail.com>
2019-07-28 12:10:03 +0200

    New NotificationsActivity Body

---
+++ app/src/main/java/com/example/easyflow/activities/NotificationsActivity.java
@@ -0,0 +1,21 @@
+package com.example.easyflow.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.easyflow.R;
+import com.example.easyflow.interfaces.NotifyEventHandler;
+
+public class NotificationsActivity extends AppCompatActivity implements NotifyEventHandler {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifications1);
+    }
+
+    @Override
+    public void Notify() {
+
+    }
+}
